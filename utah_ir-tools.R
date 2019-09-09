e4a1c58488e05a9f4f68ebc0b5a76f8a01613087
Jones <Emma.Jones@deq.virginia.gov>
2019-09-09 16:04:03 -0400

    some data in

---
+++ utah_ir-tools.R
@@ -0,0 +1,7 @@
+# utah ir package
+library(devtools)
+
+devtools::install_github("ut-ir-tools/irTools")
+
+# automatically installs wqTools but just in case here is code
+#devtools::install_github("ut-ir-tools/wqTools")
